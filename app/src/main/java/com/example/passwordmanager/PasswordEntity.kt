d2fc152e2cb84939a7b7244e750f76bab7b09987
bnkoeppen <bnk27@case.edu>
2025-04-16 14:55:38 -0400

    make id autoincrement

--- app/src/main/java/com/example/passwordmanager/PasswordEntity.kt
+++ app/src/main/java/com/example/passwordmanager/PasswordEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.*
 
 @Entity
 data class PasswordEntity (
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "site_name") val siteName: String,
     @ColumnInfo(name = "username") val username: String,
     @ColumnInfo(name = "password") val password: String
